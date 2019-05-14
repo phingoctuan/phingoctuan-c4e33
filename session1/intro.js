780bcf4441878ca5fa630729af2f89ee7fcd6d6c
phingoctuan <phingoctuan12091999@gmail.com>
2019-05-14 22:11:58 +0700

    open my folder

---
+++ session1/intro.js
@@ -0,0 +1,21 @@
+// let name = 1;
+// name = 5;
+// console.log("Hello", name);
+
+// let x = Number(prompt("Nhập số thứ nhất"));
+// let y = Number(prompt("Nhập số thứ 2")); 
+// console.log(x + y);
+
+clear()
+for(let i =0; i<100; i++){
+    fd(100)
+    rt(90)
+    fd(100)
+    rt(90)
+    fd(100)
+    rt(90)
+    fd(100)
+    rt(90)
+    
+    rt(30)
+}
